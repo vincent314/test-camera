71f5654580e73f3d476a62b5578aab2ceb43b871
Vincent Marmin <vincent.marmin@davidson.fr>
2016-09-25 21:41:59 +0200

    chore(dist): create gh-pages

---
+++ main.bundle.js
@@ -0,0 +1,572 @@
+var ac_main =
+webpackJsonpac__name_([1],{
+
+/***/ "./node_modules/css-loader/index.js!./src/app/app.style.css":
+/***/ function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("./node_modules/css-loader/lib/css-base.js")();
+// imports
+
+
+// module
+exports.push([module.i, "html, body{\n  height: 100%;\n  font-family: Arial, Helvetica, sans-serif\n}\n\nspan.active {\n  background-color: gray;\n}\n", ""]);
+
+// exports
+
+
+/***/ },
+
+/***/ "./node_modules/css-loader/lib/css-base.js":
+/***/ function(module, exports) {
+
+/*
+	MIT License http://www.opensource.org/licenses/mit-license.php
+	Author Tobias Koppers @sokra
+*/
+// css base code, injected by the css-loader
+module.exports = function() {
+	var list = [];
+
+	// return the list of modules as css string
+	list.toString = function toString() {
+		var result = [];
+		for(var i = 0; i < this.length; i++) {
+			var item = this[i];
+			if(item[2]) {
+				result.push("@media " + item[2] + "{" + item[1] + "}");
+			} else {
+				result.push(item[1]);
+			}
+		}
+		return result.join("");
+	};
+
+	// import a list of modules into the list
+	list.i = function(modules, mediaQuery) {
+		if(typeof modules === "string")
+			modules = [[null, modules, ""]];
+		var alreadyImportedModules = {};
+		for(var i = 0; i < this.length; i++) {
+			var id = this[i][0];
+			if(typeof id === "number")
+				alreadyImportedModules[id] = true;
+		}
+		for(i = 0; i < modules.length; i++) {
+			var item = modules[i];
+			// skip already imported module
+			// this implementation is not 100% perfect for weird media query combinations
+			//  when a module is imported multiple times with different media queries.
+			//  I hope this will never occur (Hey this way we have smaller bundles)
+			if(typeof item[0] !== "number" || !alreadyImportedModules[item[0]]) {
+				if(mediaQuery && !item[2]) {
+					item[2] = mediaQuery;
+				} else if(mediaQuery) {
+					item[2] = "(" + item[2] + ") and (" + mediaQuery + ")";
+				}
+				list.push(item);
+			}
+		}
+	};
+	return list;
+};
+
+
+/***/ },
+
+/***/ "./node_modules/rxjs/add/observable/of.js":
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+var Observable_1 = __webpack_require__("./node_modules/rxjs/Observable.js");
+var of_1 = __webpack_require__("./node_modules/rxjs/observable/of.js");
+Observable_1.Observable.of = of_1.of;
+//# sourceMappingURL=of.js.map
+
+/***/ },
+
+/***/ "./src/app/app.component.ts":
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+/*
+ * Angular 2 decorators and services
+ */
+var core_1 = __webpack_require__("./node_modules/@angular/core/index.js");
+var app_service_1 = __webpack_require__("./src/app/app.service.ts");
+/*
+ * App Component
+ * Top Level Component
+ */
+var App = (function () {
+    function App(appState) {
+        this.appState = appState;
+        this.angularclassLogo = 'assets/img/angularclass-avatar.png';
+        this.name = 'Angular 2 Webpack Starter';
+        this.url = 'https://twitter.com/AngularClass';
+    }
+    App.prototype.ngOnInit = function () {
+        console.log('Initial App State', this.appState.state);
+    };
+    App = __decorate([
+        core_1.Component({
+            selector: 'app',
+            encapsulation: core_1.ViewEncapsulation.None,
+            styles: [
+                __webpack_require__("./src/app/app.style.css")
+            ],
+            template: "\n    <nav>\n      <span>\n        <a [routerLink]=\" ['./'] \">\n          Index\n        </a>\n      </span>\n      |\n      <span>\n        <a [routerLink]=\" ['./home'] \">\n          Home\n        </a>\n      </span>\n      |\n      <span>\n        <a [routerLink]=\" ['./detail'] \">\n          Detail\n        </a>\n      </span>\n      |\n      <span>\n        <a [routerLink]=\" ['./about'] \">\n          About\n        </a>\n      </span>\n    </nav>\n\n    <main>\n      <router-outlet></router-outlet>\n    </main>\n\n    <pre class=\"app-state\">this.appState.state = {{ appState.state | json }}</pre>\n\n    <footer>\n      <span>WebPack Angular 2 Starter by <a [href]=\"url\">@AngularClass</a></span>\n      <div>\n        <a [href]=\"url\">\n          <img [src]=\"angularclassLogo\" width=\"25%\">\n        </a>\n      </div>\n    </footer>\n  "
+        }), 
+        __metadata('design:paramtypes', [(typeof (_a = typeof app_service_1.AppState !== 'undefined' && app_service_1.AppState) === 'function' && _a) || Object])
+    ], App);
+    return App;
+    var _a;
+}());
+exports.App = App;
+/*
+ * Please review the https://github.com/AngularClass/angular2-examples/ repo for
+ * more angular app examples that you may copy/paste
+ * (The examples may not be updated as quickly. Please open an issue on github for us to update it)
+ * For help or questions please contact us at @AngularClass on twitter
+ * or our chat on Slack at https://AngularClass.com/slack-join
+ */
+
+
+/***/ },
+
+/***/ "./src/app/app.module.ts":
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+var core_1 = __webpack_require__("./node_modules/@angular/core/index.js");
+var platform_browser_1 = __webpack_require__("./node_modules/@angular/platform-browser/index.js");
+var forms_1 = __webpack_require__("./node_modules/@angular/forms/index.js");
+var http_1 = __webpack_require__("./node_modules/@angular/http/index.js");
+var router_1 = __webpack_require__("./node_modules/@angular/router/index.js");
+var hmr_1 = __webpack_require__("./node_modules/@angularclass/hmr/dist/index.js");
+/*
+ * Platform and Environment providers/directives/pipes
+ */
+var environment_1 = __webpack_require__("./src/app/environment.ts");
+var app_routes_1 = __webpack_require__("./src/app/app.routes.ts");
+// App is our top level component
+var app_component_1 = __webpack_require__("./src/app/app.component.ts");
+var app_resolver_1 = __webpack_require__("./src/app/app.resolver.ts");
+var app_service_1 = __webpack_require__("./src/app/app.service.ts");
+var no_content_1 = __webpack_require__("./src/app/no-content/index.ts");
+var x_large_1 = __webpack_require__("./src/app/home/x-large/index.ts");
+var camera_component_1 = __webpack_require__("./src/app/camera/camera.component.ts");
+// Application wide providers
+var APP_PROVIDERS = app_resolver_1.APP_RESOLVER_PROVIDERS.concat([
+    app_service_1.AppState
+]);
+/**
+ * `AppModule` is the main entry point into Angular2's bootstraping process
+ */
+var AppModule = (function () {
+    function AppModule(appRef, appState) {
+        this.appRef = appRef;
+        this.appState = appState;
+    }
+    AppModule.prototype.hmrOnInit = function (store) {
+        if (!store || !store.state)
+            return;
+        console.log('HMR store', JSON.stringify(store, null, 2));
+        // set state
+        this.appState._state = store.state;
+        // set input values
+        if ('restoreInputValues' in store) {
+            var restoreInputValues = store.restoreInputValues;
+            setTimeout(restoreInputValues);
+        }
+        this.appRef.tick();
+        delete store.state;
+        delete store.restoreInputValues;
+    };
+    AppModule.prototype.hmrOnDestroy = function (store) {
+        var cmpLocation = this.appRef.components.map(function (cmp) { return cmp.location.nativeElement; });
+        // save state
+        var state = this.appState._state;
+        store.state = state;
+        // recreate root elements
+        store.disposeOldHosts = hmr_1.createNewHosts(cmpLocation);
+        // save input values
+        store.restoreInputValues = hmr_1.createInputTransfer();
+        // remove styles
+        hmr_1.removeNgStyles();
+    };
+    AppModule.prototype.hmrAfterDestroy = function (store) {
+        // display new elements
+        store.disposeOldHosts();
+        delete store.disposeOldHosts;
+    };
+    AppModule = __decorate([
+        core_1.NgModule({
+            bootstrap: [app_component_1.App],
+            declarations: [
+                app_component_1.App,
+                camera_component_1.CameraComponent,
+                no_content_1.NoContent,
+                x_large_1.XLarge
+            ],
+            imports: [
+                platform_browser_1.BrowserModule,
+                forms_1.FormsModule,
+                http_1.HttpModule,
+                router_1.RouterModule.forRoot(app_routes_1.ROUTES, { useHash: true })
+            ],
+            providers: [
+                environment_1.ENV_PROVIDERS,
+                APP_PROVIDERS
+            ]
+        }), 
+        __metadata('design:paramtypes', [(typeof (_a = typeof core_1.ApplicationRef !== 'undefined' && core_1.ApplicationRef) === 'function' && _a) || Object, (typeof (_b = typeof app_service_1.AppState !== 'undefined' && app_service_1.AppState) === 'function' && _b) || Object])
+    ], AppModule);
+    return AppModule;
+    var _a, _b;
+}());
+exports.AppModule = AppModule;
+
+
+/***/ },
+
+/***/ "./src/app/app.resolver.ts":
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+var core_1 = __webpack_require__("./node_modules/@angular/core/index.js");
+var Observable_1 = __webpack_require__("./node_modules/rxjs/Observable.js");
+__webpack_require__("./node_modules/rxjs/add/observable/of.js");
+var DataResolver = (function () {
+    function DataResolver() {
+    }
+    DataResolver.prototype.resolve = function (route, state) {
+        return Observable_1.Observable.of({ res: 'I am data' });
+    };
+    DataResolver = __decorate([
+        core_1.Injectable(), 
+        __metadata('design:paramtypes', [])
+    ], DataResolver);
+    return DataResolver;
+}());
+exports.DataResolver = DataResolver;
+// an array of services to resolve routes with data
+exports.APP_RESOLVER_PROVIDERS = [
+    DataResolver
+];
+
+
+/***/ },
+
+/***/ "./src/app/app.routes.ts":
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+var no_content_1 = __webpack_require__("./src/app/no-content/index.ts");
+var camera_component_1 = __webpack_require__("./src/app/camera/camera.component.ts");
+exports.ROUTES = [
+    { path: '', component: camera_component_1.CameraComponent },
+    // { path: 'home',  component: Home },
+    // { path: 'about', component: About },
+    // {
+    //   path: 'detail', loadChildren: () => System.import('./+detail').then(mod => (mod.__esModule && mod.default) ? mod.default : mod)
+    // },
+    { path: '**', component: no_content_1.NoContent },
+];
+
+
+/***/ },
+
+/***/ "./src/app/app.service.ts":
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+var core_1 = __webpack_require__("./node_modules/@angular/core/index.js");
+var AppState = (function () {
+    function AppState() {
+        this._state = {};
+    }
+    Object.defineProperty(AppState.prototype, "state", {
+        // already return a clone of the current state
+        get: function () {
+            return this._state = this._clone(this._state);
+        },
+        // never allow mutation
+        set: function (value) {
+            throw new Error('do not mutate the `.state` directly');
+        },
+        enumerable: true,
+        configurable: true
+    });
+    AppState.prototype.get = function (prop) {
+        // use our state getter for the clone
+        var state = this.state;
+        return state.hasOwnProperty(prop) ? state[prop] : state;
+    };
+    AppState.prototype.set = function (prop, value) {
+        // internally mutate our state
+        return this._state[prop] = value;
+    };
+    AppState.prototype._clone = function (object) {
+        // simple object clone
+        return JSON.parse(JSON.stringify(object));
+    };
+    AppState = __decorate([
+        core_1.Injectable(), 
+        __metadata('design:paramtypes', [])
+    ], AppState);
+    return AppState;
+}());
+exports.AppState = AppState;
+
+
+/***/ },
+
+/***/ "./src/app/app.style.css":
+/***/ function(module, exports, __webpack_require__) {
+
+
+        var result = __webpack_require__("./node_modules/css-loader/index.js!./src/app/app.style.css");
+
+        if (typeof result === "string") {
+            module.exports = result;
+        } else {
+            module.exports = result.toString();
+        }
+    
+
+/***/ },
+
+/***/ "./src/app/camera/camera.component.html":
+/***/ function(module, exports) {
+
+module.exports = "<div>\n  <h1>Test camera</h1>\n  <form>\n    <input type=\"file\" capture=\"camera\" accept=\"image/*\" id=\"cameraInput\" name=\"cameraInput\">\n    <input type=\"submit\" value=\"SUBMIT\">\n  </form>\n\n</div>\n"
+
+/***/ },
+
+/***/ "./src/app/camera/camera.component.ts":
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+var core_1 = __webpack_require__("./node_modules/@angular/core/index.js");
+var CameraComponent = (function () {
+    function CameraComponent() {
+    }
+    CameraComponent = __decorate([
+        core_1.Component({
+            selector: 'test-camera',
+            template: __webpack_require__("./src/app/camera/camera.component.html")
+        }), 
+        __metadata('design:paramtypes', [])
+    ], CameraComponent);
+    return CameraComponent;
+}());
+exports.CameraComponent = CameraComponent;
+
+
+/***/ },
+
+/***/ "./src/app/environment.ts":
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+// Angular 2
+// rc2 workaround
+var platform_browser_1 = __webpack_require__("./node_modules/@angular/platform-browser/index.js");
+var core_1 = __webpack_require__("./node_modules/@angular/core/index.js");
+// Environment Providers
+var PROVIDERS = [];
+// Angular debug tools in the dev console
+// https://github.com/angular/angular/blob/86405345b781a9dc2438c0fbe3e9409245647019/TOOLS_JS.md
+var _decorateModuleRef = function identity(value) { return value; };
+if (false) {
+    // Production
+    platform_browser_1.disableDebugTools();
+    core_1.enableProdMode();
+    PROVIDERS = PROVIDERS.slice();
+}
+else {
+    _decorateModuleRef = function (modRef) {
+        var appRef = modRef.injector.get(core_1.ApplicationRef);
+        var cmpRef = appRef.components[0];
+        var _ng = window.ng;
+        platform_browser_1.enableDebugTools(cmpRef);
+        window.ng.probe = _ng.probe;
+        window.ng.coreTokens = _ng.coreTokens;
+        return modRef;
+    };
+    // Development
+    PROVIDERS = PROVIDERS.slice();
+}
+exports.decorateModuleRef = _decorateModuleRef;
+exports.ENV_PROVIDERS = PROVIDERS.slice();
+
+
+/***/ },
+
+/***/ "./src/app/home/x-large/index.ts":
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+__export(__webpack_require__("./src/app/home/x-large/x-large.directive.ts"));
+
+
+/***/ },
+
+/***/ "./src/app/home/x-large/x-large.directive.ts":
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+var core_1 = __webpack_require__("./node_modules/@angular/core/index.js");
+/*
+ * Directive
+ * XLarge is a simple directive to show how one is made
+ */
+var XLarge = (function () {
+    function XLarge(element, renderer) {
+        // simple DOM manipulation to set font size to x-large
+        // `nativeElement` is the direct reference to the DOM element
+        // element.nativeElement.style.fontSize = 'x-large';
+        // for server/webworker support use the renderer
+        renderer.setElementStyle(element.nativeElement, 'fontSize', 'x-large');
+    }
+    XLarge = __decorate([
+        core_1.Directive({
+            selector: '[x-large]' // using [ ] means selecting attributes
+        }), 
+        __metadata('design:paramtypes', [(typeof (_a = typeof core_1.ElementRef !== 'undefined' && core_1.ElementRef) === 'function' && _a) || Object, (typeof (_b = typeof core_1.Renderer !== 'undefined' && core_1.Renderer) === 'function' && _b) || Object])
+    ], XLarge);
+    return XLarge;
+    var _a, _b;
+}());
+exports.XLarge = XLarge;
+
+
+/***/ },
+
+/***/ "./src/app/index.ts":
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+// App
+__export(__webpack_require__("./src/app/app.module.ts"));
+
+
+/***/ },
+
+/***/ "./src/app/no-content/index.ts":
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+__export(__webpack_require__("./src/app/no-content/no-content.ts"));
+
+
+/***/ },
+
+/***/ "./src/app/no-content/no-content.ts":
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+var core_1 = __webpack_require__("./node_modules/@angular/core/index.js");
+var NoContent = (function () {
+    function NoContent() {
+    }
+    NoContent = __decorate([
+        core_1.Component({
+            selector: 'no-content',
+            template: "\n    <div>\n      <h1>404: page missing</h1>\n    </div>\n  "
+        }), 
+        __metadata('design:paramtypes', [])
+    ], NoContent);
+    return NoContent;
+}());
+exports.NoContent = NoContent;
+
+
+/***/ },
+
+/***/ "./src/main.browser.ts":
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+"use strict";
+/*
+ * Angular bootstraping
+ */
+var platform_browser_dynamic_1 = __webpack_require__("./node_modules/@angular/platform-browser-dynamic/index.js");
+var environment_1 = __webpack_require__("./src/app/environment.ts");
+var hmr_1 = __webpack_require__("./node_modules/@angularclass/hmr/dist/index.js");
+/*
+ * App Module
+ * our top level module that holds all of our components
+ */
+var app_1 = __webpack_require__("./src/app/index.ts");
+/*
+ * Bootstrap our Angular app with a top level NgModule
+ */
+function main() {
+    return platform_browser_dynamic_1.platformBrowserDynamic()
+        .bootstrapModule(app_1.AppModule).then(function(MODULE_REF) {
+  if (false) {
+    module["hot"]["accept"]();
+    
+    if (MODULE_REF.instance["hmrOnInit"]) {
+      module["hot"]["data"] && MODULE_REF.instance["hmrOnInit"](module["hot"]["data"]);
+    }
+    if (MODULE_REF.instance["hmrOnStatus"]) {
+      module["hot"]["apply"](function(status) {
+        MODULE_REF.instance["hmrOnStatus"](status);
+      });
+    }
+    if (MODULE_REF.instance["hmrOnCheck"]) {
+      module["hot"]["check"](function(err, outdatedModules) {
+        MODULE_REF.instance["hmrOnCheck"](err, outdatedModules);
+      });
+    }
+    if (MODULE_REF.instance["hmrOnDecline"]) {
+      module["hot"]["decline"](function(dependencies) {
+        MODULE_REF.instance["hmrOnDecline"](dependencies);
+      });
+    }
+    module["hot"]["dispose"](function(store) {
+      MODULE_REF.instance["hmrOnDestroy"] && MODULE_REF.instance["hmrOnDestroy"](store);
+      MODULE_REF.destroy();
+      MODULE_REF.instance["hmrAfterDestroy"] && MODULE_REF.instance["hmrAfterDestroy"](store);
+    });
+  }
+  return MODULE_REF;
+})
+        .then(environment_1.decorateModuleRef)
+        .catch(function (err) { return console.error(err); });
+}
+exports.main = main;
+// needed for hmr
+// in prod this is replace for document ready
+hmr_1.bootloader(main);
+
+
+/***/ }
+
+},["./src/main.browser.ts"]);
+//# sourceMappingURL=main.map
